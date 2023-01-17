1ba389504007d8402fedbff28d7d12803e35d0dd
artiely <1119696785@qq.com>
2023-01-17 18:04:41 +0800

    first commit

---
+++ ping_pong.js
@@ -0,0 +1,62 @@
+#!/usr/local/bin/node
+
+// Might be good to use an explicit path to node on the shebang line in case
+// it isn't in PATH when launched by Chrome.
+
+var fs = require('fs');
+const cp = require("child_process");
+// const execFile  = cp.execFile;
+// 打开应用并传递端口
+cp.exec('open /Users/tanjie/Desktop/ping_pong.app --args 3000')
+var nativeMessage = require('chrome-native-messaging');
+
+var input = new nativeMessage.Input();
+var transform = new nativeMessage.Transform(messageHandler);
+var output = new nativeMessage.Output();
+
+process.stdin
+    .pipe(input)
+    .pipe(transform)
+    .pipe(output)
+    .pipe(process.stdout)
+;
+
+var subscriptions = {};
+
+var timer = setInterval(function() {
+    if (subscriptions.time) {
+        output.write({ time: new Date().toISOString() });
+    }
+}, 1000);
+
+input.on('end', function() {
+    clearInterval(timer);
+});
+
+function messageHandler(msg, push, done) {
+    if (msg.readdir) {
+        fs.readdir(msg.readdir, function(err, files) {
+            if (err) {
+                push({ error: err.message || err });
+            } else {
+                files.forEach(function(file) {
+                    push({ file: file });
+                });
+            }
+
+            done();
+        });
+    } else if (msg.subscribe) {
+        subscriptions[msg.subscribe] = true;
+        push({ subscribed: msg.subscribe });
+        done();
+    } else if (msg.unsubscribe) {
+        delete subscriptions[msg.unsubscribe];
+        push({ unsubscribed: msg.unsubscribe });
+        done();
+    } else {
+        // Just echo the message:
+        push(msg);
+        done();
+    }
+}
